0511913b24ed8678232511d37d45e0f497ec1dba
root <root@LAPTOP-J7G6LA3K.localdomain>
2020-05-25 03:06:03 +0530

    first-commit

---
+++ audio-wave/js/script.js
@@ -0,0 +1,46 @@
+
+
+
+const cards =  [
+  { title: 'sid', audiosrc: '../audio/sid-audio.mp3',bottom:'0px',text:"1"},
+  { title: 'harsh', audiosrc: '../audio/harsh-audio.mp3',bottom:'0px',text:"2"}
+];
+
+new Vue({
+  el: '#app',
+  data: {
+    cards : cards
+  },
+  methods:{
+    playSelected(item){
+       console.log(item);
+       waveIdMap[item].playPause();
+    },
+    verifyGuessedName(a,b,i){
+      if(a===b.value){
+        this.cards[i].bottom = '-200px';
+        //this.cards[i].bottom = '-200px';
+      }
+    },
+    computedBottom(i){
+      return this.cards[i].bottom;
+    }
+    }
+  });
+  var waveIdMap = [];
+  cards.map(msg=>{
+    msg.wavesurfer  = WaveSurfer.create({
+        container: '#'+msg.title,
+        waveColor: 'violet',
+        progressColor: 'purple'
+    });
+    msg.wavesurfer.load('../audio/'+msg.audiosrc);
+    waveIdMap[msg.title] = msg.wavesurfer;
+    // var p = document.getElementById(msg.title+'-play-btn');
+    // p.setAttribute('wave-ref',msg.wavesurfer);
+  });
+  
+  
+  
+
+
