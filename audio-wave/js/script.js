35fb9e24941ee6c460353e763c0cb50ceb4e316a
root <root@LAPTOP-J7G6LA3K.localdomain>
2020-05-29 03:06:16 +0530

    added audios

--- audio-wave/js/script.js
+++ audio-wave/js/script.js
@@ -2,22 +2,18 @@
 
 
 const cards =  [
-  { title: 'sid', audiosrc: '/audio-wave/audio/sid-audio.mp3',bottom:'0px',text:"Sid's Personalized Message / A clue to something"},
-  { title: 'harsh', audiosrc: '/audio-wave/audio/harsh-audio.mp3',bottom:'0px',text:"harsh's Personalized Message / A clue to something"},
-  { title: 'deepanjali', audiosrc: '/audio-wave/audio/sid-audio.mp3',bottom:'0px',text:"deepanjali's Personalized Message / A clue to something"},
-  { title: 'ipshita', audiosrc: '/audio-wave/audio/harsh-audio.mp3',bottom:'0px',text:"ipshita's Personalized Message / A clue to something"},
-  { title: 'soni', audiosrc: '/audio-wave/audio/sid-audio.mp3',bottom:'0px',text:"soni's Personalized Message / A clue to something"},
-  { title: 'mom', audiosrc: '/audio-wave/audio/harsh-audio.mp3',bottom:'0px',text:"mom's Personalized Message / A clue to something"},
-  { title: 'dad', audiosrc: '/audio-wave/audio/sid-audio.mp3',bottom:'0px',text:"dad's Personalized Message / A clue to something"},
-  { title: 'dj', audiosrc: '/audio-wave/audio/harsh-audio.mp3',bottom:'0px',text:"dj's Personalized Message / A clue to something"},
-  { title: 'sham', audiosrc: '/audio-wave/audio/sid-audio.mp3',bottom:'0px',text:"sham's Personalized Message / A clue to something"},
-  { title: 'doctor', audiosrc: '/audio-wave/audio/harsh-audio.mp3',bottom:'0px',text:"Sid's Personalized Message / A clue to something"},
-  { title: 'priyanka', audiosrc: '/audio-wave/audio/sid-audio.mp3',bottom:'0px',text:"priyanka's Personalized Message / A clue to something"},
-  { title: 'dd', audiosrc: '/audio-wave/audio/harsh-audio.mp3',bottom:'0px',text:"dd's Personalized Message / A clue to something"},
-  { title: 'shamna', audiosrc: '/audio-wave/audio/sid-audio.mp3',bottom:'0px',text:"shamna's Personalized Message / A clue to something"},
-  { title: 'rahul', audiosrc: '/audio-wave/audio/harsh-audio.mp3',bottom:'0px',text:"rahul's Personalized Message / A clue to something"},
-  { title: 'aleesha', audiosrc: '/audio-wave/audio/sid-audio.mp3',bottom:'0px',text:"aleesha's Personalized Message / A clue to something"},
-  { title: 'vishnupriya', audiosrc: '/audio-wave/audio/harsh-audio.mp3',bottom:'0px',text:"vishnupriya's Personalized Message / A clue to something"},
+  { title: 'Aleesha', audiosrc: '/audio-wave/audio/Aleesha',bottom:'0px',text:"/audio-wave/static/img/dummy.webp" },
+  { title: 'Bhuvana', audiosrc: '/audio-wave/audio/Bhuvana.mp3',bottom:'0px',text:"harsh's Personalized Message / A clue to something"},
+  { title: 'Ipshita', audiosrc: '/audio-wave/audio/Ipshita.m4a',bottom:'0px',text:"deepanjali's Personalized Message / A clue to something"},
+  { title: 'Lilesh', audiosrc: '/audio-wave/audio/Lilesh.mpeg',bottom:'0px',text:"soni's Personalized Message / A clue to something"},
+  { title: 'Ojas', audiosrc: '/audio-wave/audio/Ojas_',bottom:'0px',text:"Sid's Personalized Message / A clue to something"},
+  { title: 'Priyanka', audiosrc: '/audio-wave/audio/Priyanka',bottom:'0px',text:"priyanka's Personalized Message / A clue to something"},
+  { title: 'Rishabh', audiosrc: '/audio-wave/audio/Rishabh.m4a',bottom:'0px',text:"dd's Personalized Message / A clue to something"},
+  { title: 'Roshni', audiosrc: '/audio-wave/audio/Roshni.mp3',bottom:'0px',text:"shamna's Personalized Message / A clue to something"},
+  { title: 'Shamna', audiosrc: '/audio-wave/audio/Shamna.m4a',bottom:'0px',text:"rahul's Personalized Message / A clue to something"},
+  { title: 'Supriya', audiosrc: '/audio-wave/audio/Supriya',bottom:'0px',text:"aleesha's Personalized Message / A clue to something"},
+  { title: 'Vaishali', audiosrc: '/audio-wave/audio/Vaishali.aac',bottom:'0px',text:"vishnupriya's Personalized Message / A clue to something"},
+  { title: 'Vandana', audiosrc: '/audio-wave/audio/Vandana',bottom:'0px',text:"vishnupriya's Personalized Message / A clue to something"},
   
 
   
@@ -34,9 +30,13 @@ new Vue({
        waveIdMap[item].playPause();
     },
     verifyGuessedName(a,b,i){
-      if(a===b.value){
-        this.cards[i].bottom = '-200px';
+      if(a.toUpperCase() === b.value.toUpperCase()){
+        waveIdMap[a].pause();
+        this.cards[i].bottom = '-500px';
+        this.cards[i].error = 'red';
         //this.cards[i].bottom = '-200px';
+      }else{
+        waveIdMap['wrong'].play();
       }
     },
     computedBottom(i){
@@ -46,6 +46,7 @@ new Vue({
   });
   var waveIdMap = [];
   cards.map(msg=>{
+    cards.error = "black";
     msg.wavesurfer  = WaveSurfer.create({
         container: '#'+msg.title,
         waveColor: 'violet',
@@ -53,9 +54,18 @@ new Vue({
     });
     msg.wavesurfer.load(msg.audiosrc);
     waveIdMap[msg.title] = msg.wavesurfer;
+    
     // var p = document.getElementById(msg.title+'-play-btn');
     // p.setAttribute('wave-ref',msg.wavesurfer);
   });
+  
+  var wrong = WaveSurfer.create({
+    container: '#hidden',
+    waveColor: 'violet',
+    progressColor: 'purple'
+});
+wrong.load('/audio-wave/audio/wrong-ans.mp3');
+waveIdMap['wrong'] = wrong;
   
   
   
