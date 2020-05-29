31a327ad5028f12e5fc36ea2779b6c89d3025073
root <root@LAPTOP-J7G6LA3K.localdomain>
2020-05-29 21:04:10 +0530

    added nisha mam

--- audio-wave/js/script.js
+++ audio-wave/js/script.js
@@ -11,14 +11,15 @@ const cards =  [
   { title: 'Supriya', audiosrc: '/audio-wave/audio/Supriya',bottom:'0px',text:"" ,"img":"/audio-wave/static/img/supriya.jpeg"},
   { title: 'Rishabh', audiosrc: '/audio-wave/audio/Rishabh.m4a',bottom:'0px',text:"Clue 4 :  A fairy tale ? " ,"img":false},
   { title: 'Shamna', audiosrc: '/audio-wave/audio/Shamna.m4a',bottom:'0px',text:"" ,"img":"/audio-wave/static/img/shamna.jpeg"},
-  { title: 'Roshni', audiosrc: '/audio-wave/audio/Roshni.mp3',bottom:'0px',text:"Clue 5 : The one you trust has all the answers!" ,"img":false},
+  { title: 'Roshni', audiosrc: '/audio-wave/audio/Roshni.mp3',bottom:'0px',text:"Clue 5 : The one closest to you has all the answers!" ,"img":false},
   { title: 'Vaishali', audiosrc: '/audio-wave/audio/Vaishali.aac',bottom:'0px',text:"" ,"img":"/audio-wave/static/img/vaishali.jpeg"},
   { title: 'Vandana', audiosrc: '/audio-wave/audio/Vandana',bottom:'0px',text:"Ok Enough! Not all messages are clues!" ,"img":false},
   { title: 'Vishnu', audiosrc: '/audio-wave/audio/Vishnu',bottom:'0px',text:"" ,"img":"/audio-wave/static/img/vishnu.jpg"},
-  {title: 'Deepa', audiosrc: '/audio-wave/audio/Deepa.aac',bottom:'0px',text:"Something important coming up.." ,"img":false},
+  {title: 'Deepa', audiosrc: '/audio-wave/audio/Deepa.aac',bottom:'0px',text:"But why the hell are you searching for clues ??" ,"img":false},
   { title: 'Apurva', audiosrc: '/audio-wave/audio/Apurva',bottom:'0px',text:"" ,"img":"/audio-wave/static/img/apurva-pk.jpeg"},  
-  {title:"Kirti", audiosrc: '/audio-wave/audio/kirti.m4a',bottom:'0px',text:"A trip to US ?? Enjoy the lockdown!!" ,"img":false},
+  {title:"Kirti", audiosrc: '/audio-wave/audio/kirti.m4a',bottom:'0px',text:"Do you expect a gift? :P" ,"img":false},
   { title: 'Deepanjali', audiosrc: '/audio-wave/audio/deepanjali.ogg',bottom:'0px',text:"" ,"img":"/audio-wave/static/img/deepanjali.jpeg"},  
+  {title:"nishamam", audiosrc: '/audio-wave/audio/nishamam.ogg',bottom:'0px',text:"" ,"img":"/audio-wave/static/img/nisha-mam.jfif"},
   {title:"Sid", audiosrc: '/audio-wave/audio/Sid.aac',bottom:'0px',text:"" ,"img":"/audio-wave/static/img/sid.jpg"}
   
 
