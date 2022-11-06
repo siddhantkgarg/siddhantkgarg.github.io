0d9a75523ec3e6d25a53ab791cdb06d90f4a0ec1
siddhantkgarg <siddhantkgarg@gmail.com>
2022-11-07 02:24:27 +0530

    keeping only bikepackergo in index

--- urbanstraw/gulpfile.js
+++
@@ -1,9 +0,0 @@
-var gulp = require('gulp'),
-  bundle = require('gulp-bundle-assets'),
-  clean  = require('gulp-dest-clean');
-gulp.task('bundle', function() {
-  return gulp.src('./bundle.config.js')
-    .pipe(bundle())
-    .pipe(clean("./public/dist",'*'))
-    .pipe(gulp.dest('./public/dist/'));
-});
